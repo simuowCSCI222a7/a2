047a390fb1002c245b1ef5d51434fae19f9d5043
vmw_ubuntu <vmw_ubuntu@vmwubuntu>
2018-02-12 13:42:11 +0800

    class created

---
+++ testClass.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   testClass.h
+ * Author: vmw_ubuntu
+ *
+ * Created on February 12, 2018, 1:41 PM
+ */
+
+#ifndef TESTCLASS_H
+#define TESTCLASS_H
+
+class testClass {
+public:
+    testClass();
+    testClass(const testClass& orig);
+    virtual ~testClass();
+private:
+
+};
+
+#endif /* TESTCLASS_H */
+
